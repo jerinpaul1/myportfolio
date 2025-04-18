6e026fdb4913e97e607ca1a310c53feb8f1bd827
jerinpaul1 <jerinpaul0208@gmail.com>
2025-04-18 14:37:55 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Simple animation trigger
+document.addEventListener("DOMContentLoaded", () => {
+  const fades = document.querySelectorAll('.fade-in');
+  fades.forEach(el => el.style.opacity = 1);
+});
